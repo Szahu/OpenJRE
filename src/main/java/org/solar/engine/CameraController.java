a9045b6ee19992f977c053b750712268f06a96d8
Danil <d.a.bren@student.utwente.nl>
2021-11-12 21:34:57 +0100

    handeling camera

--- src/main/java/org/solar/engine/CameraController.java
+++ src/main/java/org/solar/engine/CameraController.java
@@ -21,35 +21,74 @@ public class CameraController {
     private float offsetX = 0;
     private float offsetY = 0;
     private float offsetZ = 1;
-    private final float speed = 0.01f;
+    private final float speed = 0.015f;
+
+    private float rotX = 0;
+    private float rotY = 0;
+    private float rotZ = 0;
 
     public CameraController(Consumer<Matrix4f> setTransformMatrixCallback, Supplier<Matrix4f> getTransformMatrixCallback) {
         m_setTransformMatrixCallback = setTransformMatrixCallback;
         m_getTransformMatrixCallback = getTransformMatrixCallback;
     }
 
+    private void moveLeftRight(Boolean dir) {
+        offsetY -= Math.sin(rotZ)*Math.sin(rotX)*speed*(dir?1:-1);
+        offsetX += Math.cos(rotZ)*Math.cos(rotX)*Math.cos(rotY)*speed*(dir?1:-1);
+        offsetZ -= Math.cos(rotX)*Math.sin(rotY)*speed*(dir?1:-1);
+    }
+
+    private void moveUpDown(Boolean dir) {
+
+    }
+
+    private void moveForewardBackwadr(Boolean dir) {
+        offsetY += Math.sin(rotX)*speed*(dir?1:-1) ; 
+        offsetX -= Math.cos(rotX)*Math.sin(rotY)*speed*(dir?1:-1);
+        offsetZ -= Math.cos(rotX)*Math.cos(rotY)*speed*(dir?1:-1);
+    }
+
     public void update() {
-
-        Matrix4f newTrans = new Matrix4f().identity().translate(new Vector3f(offsetX, offsetY, offsetZ));
-
+        //Matrix4f newRot = new Matrix4f().identity().translate(new Vector3f(offsetX, offsetY, offsetZ));
+        Matrix4f newTrans = new Matrix4f().identity().translate(new Vector3f(offsetX, offsetY, offsetZ)).rotateX(rotX).rotateY(rotY).rotateZ(rotZ).scale(1);
         setTransformMatrix(newTrans);
         if(Input.isKeyDown(GLFW_KEY_W)) {
-            offsetZ += speed;
+            moveForewardBackwadr(true);
         }
         if(Input.isKeyDown(GLFW_KEY_A)) {
-            offsetX -= speed;
+            moveLeftRight(false);
         }
         if(Input.isKeyDown(GLFW_KEY_S)) {
-            offsetZ -= speed;
+            moveForewardBackwadr(false); // false - move back
         }
         if(Input.isKeyDown(GLFW_KEY_D)) {
-            offsetX += speed;
+            moveLeftRight(true);
         }
         if(Input.isKeyDown(GLFW_KEY_LEFT_SHIFT)) {
             offsetY += speed;
         }
         if(Input.isKeyDown(GLFW_KEY_LEFT_CONTROL)) {
             offsetY -= speed;
+        }
+        if(Input.isKeyDown(GLFW_KEY_UP)) {
+            rotX += Math.cos(rotY)*speed;
+            rotZ -= Math.sin(rotY)*speed;
+        }
+        if(Input.isKeyDown(GLFW_KEY_LEFT)) {
+            rotY += speed;
+        }
+        if(Input.isKeyDown(GLFW_KEY_RIGHT)) {
+            rotY -= speed;
+        }
+        if(Input.isKeyDown(GLFW_KEY_DOWN)) {
+            rotX -= Math.cos(rotY)*speed;
+            rotZ += Math.sin(rotY)*speed;
+        }
+        if(Input.isKeyDown(GLFW_KEY_Q)) {
+            rotZ += speed;
+        }
+        if(Input.isKeyDown(GLFW_KEY_E)) {
+            rotZ -= speed;
         }
     }
 }
