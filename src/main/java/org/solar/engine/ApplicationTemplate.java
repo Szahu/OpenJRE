0d7478450a12d0c3e81b88f3825db9c50b48f299
stanislaw <szahuyt@gmail.com>
2021-11-20 03:13:36 +0100

    adding docs

--- src/main/java/org/solar/engine/ApplicationTemplate.java
+++
@@ -1,9 +0,0 @@
-package org.solar.engine;
-
-import java.io.IOException;
-
-public abstract class ApplicationTemplate {
-    public abstract void initialise() throws IOException;
-    public abstract void update();
-    public abstract void terminate();
-}
